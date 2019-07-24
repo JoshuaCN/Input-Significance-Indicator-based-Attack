b28b5ce2809f63d29f87f5ea1cede1be2e360262
JoshuaCN <37404028+JoshuaCN@users.noreply.github.com>
2019-07-24 14:33:44 +0800

    None

--- images/wget_imagenet_2011_samples.sh
+++ images/wget_imagenet_2011_samples.sh
@@ -2,13 +2,39 @@
 # By using this script you download image data own your own risk and responsibility.
 #
 # Imagenet 2011 images, full url list is available here: http://image-net.org/imagenet_data/urls/imagenet_winter11_urls.tgz
-wget http://farm4.static.flickr.com/3142/2592291184_71735af93e.jpg -O bell_pepper.jpg
-wget http://farm1.static.flickr.com/36/122398209_7915b8bcdb.jpg -O baseball.jpg
-wget http://farm1.static.flickr.com/165/329702023_cb41c65e84.jpg -O ice_lolly.jpg
-wget http://farm3.static.flickr.com/2207/1675176906_8f98de2f96.jpg -O Dungeness_crab.jpg
-wget http://farm2.static.flickr.com/1292/1155177675_d0d23ede2a.jpg -O broom.jpg
-wget http:// farm3.static.flickr.com/2123/2347434732_23d10f8587.jpg -O abaya.jpg
-wget https://farm1.static.flickr.com/114/289708325_4eed5e4f24.jpg -O tiger_cat.jpg
-wget http://farm1.static.flickr.com/178/377040548_70f28db9fd.jpg -O rock.jpg
-wget https://farm4.static.flickr.com/3114/2806208205_ec50bb2e68.jpg -O pencil.jpg
-wget https://farm1.static.flickr.com/25/215185355_e823d4d8ba.jpg -O computer_mouse.jpg
+
+wget http://farm3.static.flickr.com/2547/3980627577_087c199c7d.jpg
+wget http://farm3.static.flickr.com/2569/4092262572_b76cb9158a.jpg
+wget http://farm4.static.flickr.com/3170/3088985331_546087827a.jpg
+wget http://farm4.static.flickr.com/3228/2827247993_cb39969c64.jpg
+wget http://farm3.static.flickr.com/2176/2163286527_3402a3409f.jpg
+
+wget http://www.ahgi.com/Lyle/Motorcycles/Rides/images/Nebo/800/DevilsKitchen.jpg
+wget http://bp1.blogger.com/_2D34lcVlJUE/Rkxpn2P4JYI/AAAAAAAAABE/sfoKVIy9eH0/s400/erupcion.jpg
+wget http://farm1.static.flickr.com/177/364090210_8c1150a942.jpg?v=0
+wget http://hhbennettstudio.wisconsinhistory.org/About/images/about-history.jpg
+wget http://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Geological_Formations-Oregon.jpg/120px-Geological_Formations-Oregon.jpg
+
+wget http://www.landon.net/uploaded/top_banner_photos/AthleticsTemp.jpg
+wget http://www.nait.ca/athletics/images/mbasketball_main.JPG
+wget http://www.eindhovensesportraad.nl/UserFiles/Image/atletiek%20DSC_6707web.JPG
+wget http://extras.mnginteractive.com/live/media/site571/2009/0312/20090312__ecct0313athletics~1_Viewer.JPG
+wget http://www.hsc.edu/athletics/football/images/simpson_act.jpg
+
+wget http://farm4.static.flickr.com/3332/3276572160_c3f1d4acf9.jpg
+wget http://www.gift12345.com/UserDocument/tongyizhai/Picture/200782414840.jpg
+wget http://farm4.static.flickr.com/3232/3116901494_4b2c1148c0.jpg
+wget http://www.mrmodern.com/var/ezwebin_site/storage/fckeditor/Image/U2596P1T1D18801634F21DT20091010130910.jpg
+wget http://farm4.static.flickr.com/3277/2869155661_73072b83f6.jpg
+
+wget http://www.blaikiewell.com/freephotographs/fungus19.jpg
+wget http://farm4.static.flickr.com/3123/3221279663_aea5fafd54.jpg
+wget http://farm4.static.flickr.com/3001/2570658809_1982327032.jpg
+wget http://farm4.static.flickr.com/3283/3049411391_c9a3d576a2.jpg
+wget http://farm1.static.flickr.com/102/310805111_2ce3809ac9.jpg
+
+wget http://static.flickr.com/3423/3788747850_c9653099c2.jpg
+wget http://secrets-of-flirting.com/girlfriend.jpg
+wget http://z.about.com/d/kidstvmovies/1/0/a/8/sm3005.jpg
+wget http://static.flickr.com/3455/3372482944_244c25c45f.jpg
+wget http://static.flickr.com/3592/3376795744_e89f42f5c5.jpg
